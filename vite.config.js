e162b0437081c88143d4a7418c175e55d2bd38d5
Santiago Romero <sanromerito21@gmail.com>
2025-08-08 18:23:58 -0300

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Configuración para GitHub Pages
 export default defineConfig({
+  base: "/Portfolio/",
   plugins: [react()],
 })
