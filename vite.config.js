83e3a57c439646bde851fe9e19a9c9cd59e6bbe3
Santiago Romero <sanromerito21@gmail.com>
2025-08-08 18:49:01 -0300

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Portfolio/",  // ¡Exacto! Mayúsculas, minúsculas y barra final
+  base: "/https://github.com/Secreto21/Portfolio/",
   plugins: [react()]
 })
